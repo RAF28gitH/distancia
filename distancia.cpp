4210da9fbe90b302d70837f3caadc50e14449fcb
RAF28gitH <raf28elnooob@gmail.com>
2024-10-14 08:30:00 -0600

    Agregué una despedida al finalizar el código con una función que además muestra mi usuario de Codeforces y OmegaUp. Además de mini cambios más.

--- distancia.cpp
+++ distancia.cpp
@@ -6,9 +6,15 @@ struct coordenadas{
 };
 
 void presentacion(){
-    cout<<"Archivo para calcular la distancia entre dos coordenadas"<<endl;
-    cout<<"Autor: Juan Rafael Gonzalez Diaz (RAF28)"<<endl;
-    cout<<"Fecha: 14/10/2024"<<endl;
+    cout<<"Archivo para calcular la distancia entre dos coordenadas."<<endl;
+    cout<<"Autor: Juan Rafael Gonzalez Diaz (RAF28)."<<endl;
+    cout<<"Fecha: 14/10/2024."<<endl;
+}
+
+void despedida(){
+    cout<<"Gracias por usar el codigo, suerte en la vida. :D"<<endl;
+    cout<<"Codeforces: RAF28"<<endl;
+    cout<<"OmegaUp: -TyphlosionParaPresidente-"<<endl;
 }
 
 float distancia(coordenadas _c1,coordenadas _c2){
@@ -19,6 +25,7 @@ int main(){
     presentacion();
 	coordenadas c1,c2;
 	cin>>c1.x>>c1.y>>c2.x>>c2.y;
-	cout<<fixed<<setprecision(6)<<distancia(c1,c2);
+	cout<<fixed<<setprecision(6)<<"Distancia: "<<distancia(c1,c2)<<endl;
+	despedida();
 	return 0;
 }
