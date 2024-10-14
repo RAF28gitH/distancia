d8766472b434dbb03c26ca46b093bebf0ae94492
R <raf28elnooob@gmail.com>
2024-10-14 07:59:42 -0600

    Una estructura para que se vea chido

--- distancia.cpp
+++ distancia.cpp
@@ -1,13 +1,16 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-float distancia(float _x1,float _y1,float _x2,float _y2){
-    return sqrt(pow((_x2-_x1),2)+pow((_y2-_y1),2));
+struct coordenadas{
+    float x,y;
+};
+float distancia(coordenadas _c1,coordenadas _c2){
+    return sqrt(pow((_c2.x-_c1.x),2)+pow((_c2.y-_c1.y),2));
 }
 
 int main(){
-	float x1,y1,x2,y2;
-	cin>>x1>>y1>>x2>>y2;
-	cout<<fixed<<setprecision(6)<<distancia(x1,y1,x2,y2);
+	coordenadas c1,c2;
+	cin>>c1.x>>c1.y>>c2.x>>c2.y;
+	cout<<fixed<<setprecision(6)<<distancia(c1,c2);
 	return 0;
 }
